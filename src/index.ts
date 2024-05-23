4445b2c4c112867c806062b42ca9e5274f94d066
rodrigo serrano <kiradex21@gmail.com>
2024-05-22 21:06:08 -0600

    chore: add cors support & new API routes

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
 import express, { Request, Response, NextFunction } from "express";
+import { createBooks, getAllBooks } from "./controllers/booksControllers";
 
 const app = express();
 const port = process.env.PORT || 8000;
-
-// Middleware para parsear JSON
 app.use(express.json());
 
-// Ruta de ejemplo
+var cors = require("cors");
+
+const corsOpts = {
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
+};
+
+app.use(cors(corsOpts));
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript with Express!");
 });
 
-// Manejo de errores
+app.get("/api/books", getAllBooks);
+app.post("/api/createBooks", createBooks);
+
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
