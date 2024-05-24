231aa7835b33f3b59d4574454c36bd3aec7d7782
Rodricode21 <kiradex21@gmail.com>
2024-05-23 21:38:20 -0600

    chore: refactor to use environment variables

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,13 @@
 import { Pool } from "pg";
+import dotenv from 'dotenv'
+dotenv.config();
 
 const pool = new Pool({
-  user: "user",
-  host: "localhost",
-  database: "postgres",
-  password: "pg_password",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: +!process.env.DB_PORT,
 });
 
 export default pool;
